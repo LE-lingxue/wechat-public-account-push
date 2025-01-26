feb42dd96a4d8d8d98cda69c57ee75f4968a51f6
LE-lingxue <93124172+LE-lingxue@users.noreply.github.com>
2025-01-26 11:22:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,7 +7,7 @@
  */
 const USER_CONFIG = {
   // 请将以下代码按照图示粘贴到对应位置
-  USE_PASSAGE: 'push-plus',
+  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxe78489a4c8a7670d',
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '徐大乖',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ae7a9f9a91eb4203830f0707988bcb24',
+      id: 'PDU33637TT3B7SuKsyNVDVszoYM04PcacRtwICFpZ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oDOJM7E2lpJCiBUv5Zw5qMTfpu4Q1',
+      id: 'PDU33637TT3B7SuKsyNVDVszoYM04PcacRtwICFpZ',
     }
   ],
 
