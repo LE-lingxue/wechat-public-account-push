1760c3bda1b6646bbbdf743d3a6d83536010f0c3
LE-lingxue <93124172+LE-lingxue@users.noreply.github.com>
2025-01-26 10:36:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,8 +15,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'ae7a9f9a91eb4203830f0707988bcb24',
 
-  PROVINCE: '爱你省',
-  CITY: '爱你市',
+  PROVINCE: '河南省',
+  CITY: '郑州市',
 
   USERS: [
     {
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDOJM7E2lpJCiBUv5Zw5qMTfpu4Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '001',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-20',
       festivals: [
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'yuKQpTzMWaaRCzba_wepgsN6qfp3QFgmwcjda5eHtAc',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
