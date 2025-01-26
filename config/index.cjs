b5f719716a083d77537d4bd99a952f7bf0051771
LE-lingxue <93124172+LE-lingxue@users.noreply.github.com>
2025-01-26 10:32:41 +0800

    Update index.cjs
    PUSH

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,14 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-plus',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wxe78489a4c8a7670d',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '0a2c7406f7ab36dd25901c1c3176d1fd',
+  APP_SECRET: 'ae7a9f9a91eb4203830f0707988bcb24',
 
   PROVINCE: '爱你省',
   CITY: '爱你市',
@@ -23,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDOJM7E2lpJCiBUv5Zw5qMTfpu4Q',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GXIbD0cu3rrzy2bpjxB2t_epqi-UhbHQM73yJSQxPEQ',
+      useTemplateId: '001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-20',
       festivals: [
